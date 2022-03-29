182ec74e2d933e475a584cb882015c1bc96bfdb6
Tomwf <chowkirthmas@gmail.com>
2022-03-29 16:20:38 +0200

    Feat: view issues with or without filter on GET request

--- routes/api.js
+++ routes/api.js
@@ -30,7 +30,42 @@ module.exports = function (app) {
 
     .get(function (req, res){
       let project = req.params.project;
-      
+      const {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        open,
+        status_text
+      } = req.query
+
+      mongoose.connect(MONGO_URI)
+
+      Issue.find((err, docs) => {
+        if (err) console.error(err)
+
+        let result = docs
+        if (issue_title) {
+          result = result.filter(issue => issue.issue_title === issue_title)
+        }
+        if (issue_text) {
+          result = result.filter(issue => issue.issue_text === issue_text)
+        }
+        if (created_by) {
+          result = result.filter(issue => issue.created_by === created_by)
+        }
+        if (assigned_to) {
+          result = result.filter(issue => issue.assigned_to === assigned_to)
+        }
+        if (open) {
+          result = result.filter(issue => issue.open === JSON.parse(open))
+        }
+        if (status_text) {
+          result = result.filter(issue => issue.status_text === status_text)
+        }
+
+        res.json(result)
+      })
     })
 
     .post(function (req, res){
