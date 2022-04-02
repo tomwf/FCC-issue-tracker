538a351b5025ce2103c177d4ed2e1fb9ac0827d6
Tomwf <chowkirthmas@gmail.com>
2022-04-02 19:28:36 +0200

    fix: add query for _id in GET request

--- routes/api.js
+++ routes/api.js
@@ -28,6 +28,7 @@ module.exports = function (app) {
     .get(function (req, res){
       let project = req.params.project
       const {
+        _id,
         issue_title,
         issue_text,
         created_by,
@@ -49,24 +50,27 @@ module.exports = function (app) {
       Issue.find((err, docs) => {
         if (err) console.error(err)
 
-        let result = docs
+        let result = [...docs]
+        if (_id) {
+          result = result.filter(doc => doc._id.toString() === _id)
+        }
         if (issue_title) {
-          result = result.filter(issue => issue.issue_title === issue_title)
+          result = result.filter(doc => doc.issue_title === issue_title)
         }
         if (issue_text) {
-          result = result.filter(issue => issue.issue_text === issue_text)
+          result = result.filter(doc => doc.issue_text === issue_text)
         }
         if (created_by) {
-          result = result.filter(issue => issue.created_by === created_by)
+          result = result.filter(doc => doc.created_by === created_by)
         }
         if (assigned_to) {
-          result = result.filter(issue => issue.assigned_to === assigned_to)
+          result = result.filter(doc => doc.assigned_to === assigned_to)
         }
         if (open) {
-          result = result.filter(issue => issue.open === JSON.parse(open))
+          result = result.filter(doc => doc.open === JSON.parse(open))
         }
         if (status_text) {
-          result = result.filter(issue => issue.status_text === status_text)
+          result = result.filter(doc => doc.status_text === status_text)
         }
 
         res.send(result)
